28ba3ebbd0b35f29e7ccd7d34a7fddb3c756ea8a
CintyaAprilaFandini <cintyaacaf07@gmail.com>
2023-08-28 09:48:42 +0700

    perubahan admin + default

--- database/migrations/2022_07_26_081938_create_transactions_table.php
+++ database/migrations/2022_07_26_081938_create_transactions_table.php
@@ -16,19 +16,19 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_customer');
-            $table->foreign('id_customer')->references('id')->on('customer');
-            $table->unsignedBigInteger('id_media');
-            $table->foreign('id_media')->references('id')->on('media');
-            $table->unsignedBigInteger('id_service');
-            $table->foreign('id_service')->references('id')->on('service');
-            $table->unsignedBigInteger('id_purpose');
-            $table->foreign('id_purpose')->references('id')->on('purpose');
+            $table->foreign('id_customer')->references('id')->on('customer')->default("null");
+            $table->unsignedBigInteger('id_media')->default("null");
+            $table->foreign('id_media')->references('id')->on('media')->default("null");
+            $table->unsignedBigInteger('id_service')->default("null");
+            $table->foreign('id_service')->references('id')->on('service')->default("null");
+            $table->unsignedBigInteger('id_purpose')->default("null");
+            $table->foreign('id_purpose')->references('id')->on('purpose')->default("null");
             $table->string('data');
             $table->unsignedBigInteger('id_sub_categories');
             $table->foreign('id_sub_categories')->references('id')->on('sub_categories');
-            $table->unsignedBigInteger('id_tujuan_kunjungan');
+            $table->unsignedBigInteger('id_tujuan_kunjungan')->default("null");
             $table->foreign('id_tujuan_kunjungan')->references('id')->on('tujuan_kunjungans');
-            $table->unsignedBigInteger('id_nama_pegawai');
+            $table->unsignedBigInteger('id_nama_pegawai')->default("null");
             $table->foreign('id_nama_pegawai')->references('id')->on('nama_pegawai');
             $table->boolean('status')->default(0);
 
